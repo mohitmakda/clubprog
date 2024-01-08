605476f91d59a7579799602c8f241e89dffaadc3
mohit makda <mohitmakda1404@gmail.com>
2024-01-08 23:47:51 +0530

    done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,7 +34,7 @@ export const Projects = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <h2>Past-events</h2>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>Dive into the memorable experiences of our past events, captured in the posters below. Rewind and relive the highlights that made each occasion special.</p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                       {/* <Nav.Item>
